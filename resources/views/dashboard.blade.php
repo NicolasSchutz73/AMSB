e9d25f715badc542078be5f10e7b14f4e044a66c
alexvelly <117660435+alexvelly@users.noreply.github.com>
2024-01-06 00:32:36 +0100

    bouton header dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,6 +3,9 @@
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
         </h2>
+
+
+
     </x-slot>
 
     <div class="py-12">
--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,8 @@ Route::get('/calendar', function () {
     return view('calendar');
 })->middleware(['auth', 'verified'])->name('calendar');
 
+
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');

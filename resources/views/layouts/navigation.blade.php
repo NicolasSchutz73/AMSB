0a07656f25e6f336c49b9cb07c6a9b1bfe00950d
CroustiBapt <89194028+CobsiHD@users.noreply.github.com>
2024-01-06 17:55:30 +0100

    on supprime chatify 2!

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -23,11 +23,7 @@
                     </x-nav-link>
                 </div>
 
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('chatify')" :active="request()->routeIs('chatify')">
-                        {{ __('Messagerie') }}
-                    </x-nav-link>
-                </div>
+
             </div>
 
             <!-- Settings Dropdown -->

c8f34f7ede644003c72279ff250636f5e36e28b0
CroustiBapt <89194028+CobsiHD@users.noreply.github.com>
2024-01-07 03:13:31 +0100

    2e essaie pas trop concluant :
    je reprend le code de l'essai 1 mais meme avec le chat c'est super dure je comrpend pas

--- resources/js/components/Groups.vue
+++ resources/js/components/Groups.vue
@@ -1,42 +1,24 @@
-<template>
-    <div>
-        <group-chat v-for="group in groups" :group="group" :key="group.id"></group-chat>
-    </div>
-</template>
+
 
 <script>
-export default {
-    props: ['group'],
 
-    data() {
-        return {
-            conversations: [],
-            message: '',
-            group_id: this.group.id
+export default {
+    props: ['user'],
+    computed: {
+        groups() {
+            return this.$store.state.groups;
         }
     },
-
     mounted() {
-        this.listenForNewMessage();
+        this.listenForNewGroups();
     },
-
     methods: {
-        store() {
-            axios.post('/conversations', {message: this.message, group_id: this.group.id})
-                .then((response) => {
-                    this.message = '';
-                    this.conversations.push(response.data);
-                });
-        },
-
-        listenForNewMessage() {
-            Echo.private('groups.' + this.group.id)
-                .listen('NewMessage', (e) => {
-                    // console.log(e);
-                    this.conversations.push(e);
+        listenForNewGroups() {
+            Echo.private('users.' + this.user.id)
+                .listen('GroupCreated', (e) => {
+                    this.$store.dispatch('createGroup', e.group);
                 });
         }
     }
 }
 </script>
-

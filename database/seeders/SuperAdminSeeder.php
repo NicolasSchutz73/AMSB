8b185fa3a58d6bf12eaac9df54db957a4cc33029
Nicolas Schutz <90822955+NicolasSchutz73@users.noreply.github.com>
2024-01-08 14:52:14 +0100

    SuperAdminSeeder.php : ajout du super admin et d'un admin de test

--- database/seeders/SuperAdminSeeder.php
+++ database/seeders/SuperAdminSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class SuperAdminSeeder extends Seeder
 {
@@ -12,6 +14,23 @@ class SuperAdminSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Création du Super Admin
+        $superAdmin = User::create([
+            'firstname' => 'G3L3INFO',
+            'lastname' => 'AMSB',
+            'email' => 'g3l3info@gmail.com',
+            'password' => Hash::make('G3L3INFO1234')
+        ]);
+
+        $superAdmin->assignRole('Super Admin');
+
+        // Création d'un admin pour test, à enlever plus tard
+        $admin = User::create([
+            'firstname' => 'Nicolas',
+            'lastname' => 'Schutz',
+            'email' => 'nicolas.schutz@gmail.com',
+            'password' => Hash::make('ahsan1234')
+        ]);
+        $admin->assignRole('Admin');
     }
 }

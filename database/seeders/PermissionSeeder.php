794b51b859ffb778ef3be971c5a5971ad2db79a9
Nicolas Schutz <90822955+NicolasSchutz73@users.noreply.github.com>
2024-01-08 14:29:29 +0100

    Ajout des seeders pour les permissions

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
 
 class PermissionSeeder extends Seeder
 {
@@ -12,6 +13,19 @@ class PermissionSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // tableau des différentes permissions
+        $permissions = [
+            'create-role',
+            'edit-role',
+            'delete-role',
+            'create-user',
+            'edit-user',
+            'delete-user',
+        ];
+
+        // On boucle sur le tableau permissions pour les insérer dans la table des permissions
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
     }
 }

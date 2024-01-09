9c5b846a1ac5fdd620b1707c23847bc9c60707c0
simon <charriersim@gmail.com>
2024-01-09 14:14:17 +0100

    GroupChat

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-
+    Route::resource('chat', \App\Http\Controllers\ChatController::class);
 
 });
 

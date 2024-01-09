ee01c96ec65cd53bf9b39aec1f093331f1ec6436
Nicolas Schutz <90822955+NicolasSchutz73@users.noreply.github.com>
2024-01-09 10:52:46 +0100

    StoreUserRequest.php : Refactorisation des règles de validation

---
+++ app/Http/Requests/StoreUserRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUserRequest extends FormRequest
+{
+    /**
+     * Détermine si l'utilisateur est autorisé à faire cette demande.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Récupère les règles de validation applicables à la demande.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'firstname' => 'required|string|max:250',
+            'lastname' => 'required|string|max:250',
+            'email' => 'required|string|email:rfc,dns|max:250|unique:users,email',
+            'password' => 'required|string|min:8|confirmed',
+            'roles' => 'required'
+        ];
+    }
+}

57497276bbba84199f43e8f751cbfc7e1fa7955f
simon <charriersim@gmail.com>
2024-01-09 14:25:40 +0100

    GroupChat correction

--- routes/web.php
+++ routes/web.php
@@ -26,14 +26,14 @@ Route::get('/calendar', function () {
     return view('calendar');
 })->middleware(['auth', 'verified'])->name('calendar');
 
-
+Route::resource('chat', \App\Http\Controllers\ChatController::class);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::resource('chat', \App\Http\Controllers\ChatController::class);
+
 
 });
 

59b73fe6f3fdf9774af7e4a5d1bc238ec2010644
Nicolas Schutz <90822955+NicolasSchutz73@users.noreply.github.com>
2024-01-28 14:36:03 +0100

    Vue terminer pour l'index des roles

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -1,58 +1,56 @@
-<!-- TODO : modifier le style/mise en page -->
-
 <x-app-layout>
     <x-slot name="header">
-        <div class="card-header">Manage Roles</div>
-        <div class="card-body">
-            @can('create-role')
-                <a href="{{ route('roles.create') }}" class="btn btn-success btn-sm my-2"><i class="bi bi-plus-circle"></i> Add New Role</a>
-            @endcan
+        <h2 class="font-semibold text-xl text-white leading-tight">
+            Gérer les rôles
+        </h2>
     </x-slot>
+    <div class="p-6">
+        @can('create-role')
+            <a href="{{ route('roles.create') }}" class="inline-flex items-center justify-center px-4 py-2 border border-white text-white rounded-md shadow-sm hover:bg-white hover:text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white sm:ml-3 mt-3 sm:mt-0 mb-4">Add New Role</a>
+        @endcan
+            <table class="min-w-full divide-y divide-gray-200">
+                <thead class="bg-gray-50">
+                <tr>
+                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">ID</th>
+                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Nom</th>
+                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider w-1/4">Action</th>
+                </tr>
+                </thead>
+                <tbody class="bg-white divide-y divide-gray-200">
+                @forelse ($roles as $role)
+                    <tr class="hover:bg-gray-50">
+                        <td class="px-6 py-4 whitespace-nowrap">{{ $loop->iteration }}</td>
+                        <td class="px-6 py-4 whitespace-nowrap">{{ $role->name }}</td>
+                        <td class="px-6 py-4 whitespace-nowrap flex justify-between">
+                            <form action="{{ route('roles.destroy', $role->id) }}" method="post">
+                                @csrf
+                                @method('DELETE')
 
-    <table class="table table-striped table-bordered">
-        <thead>
-        <tr>
-            <th scope="col">id</th>
-            <th scope="col">Name</th>
-            <th scope="col" style="width: 250px;">Action</th>
-        </tr>
-        </thead>
-        <tbody>
-        @forelse ($roles as $role)
-            <tr>
-                <th scope="row">{{ $loop->iteration }}</th>
-                <td>{{ $role->name }}</td>
-                <td>
-                    <form action="{{ route('roles.destroy', $role->id) }}" method="post">
-                        @csrf
-                        @method('DELETE')
+                                <a href="{{ route('roles.show', $role->id) }}" class="inline-flex items-center bg-blue-500 text-white rounded-md px-4 py-2 hover:bg-blue-600 transition duration-200 ease-in-out">Voir</a>
 
-                        <a href="{{ route('roles.show', $role->id) }}" class="btn btn-warning btn-sm"><i class="bi bi-eye"></i> Show</a>
+                                @if ($role->name != 'Super Admin')
+                                    @can('edit-role')
+                                        <a href="{{ route('roles.edit', $role->id) }}" class="inline-flex items-center bg-green-500 text-white rounded-md px-4 py-2 hover:bg-green-600 transition duration-200 ease-in-out">Modifier</a>
+                                    @endcan
 
-                        @if ($role->name!='Super Admin')
-                            @can('edit-role')
-                                <a href="{{ route('roles.edit', $role->id) }}" class="btn btn-primary btn-sm"><i class="bi bi-pencil-square"></i> Edit</a>
-                            @endcan
-
-                            @can('delete-role')
-                                @if ($role->name!=Auth::user()->hasRole($role->name))
-                                    <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Do you want to delete this role?');"><i class="bi bi-trash"></i> Delete</button>
+                                    @can('delete-role')
+                                        @if ($role->name != Auth::user()->hasRole($role->name))
+                                            <button type="submit" class="inline-flex items-center bg-red-500 text-white rounded-md px-4 py-2 hover:bg-red-600 transition duration-200 ease-in-out" onclick="return confirm('Voulez-vous supprimer ce rôle ?');">Supprimer</button>
+                                        @endif
+                                    @endcan
                                 @endif
-                            @endcan
-                        @endif
-
-                    </form>
-                </td>
-            </tr>
-        @empty
-            <td colspan="3">
-                        <span class="text-danger">
-                            <strong>No Role Found!</strong>
-                        </span>
-            </td>
-        @endforelse
-        </tbody>
-    </table>
-
-    {{ $roles->links() }}
+                            </form>
+                        </td>
+                    </tr>
+                @empty
+                    <td colspan="3" class="px-6 py-4 whitespace-nowrap">
+                <span class="text-danger">
+                    <strong>Aucun rôle trouver !</strong>
+                </span>
+                    </td>
+                @endforelse
+                </tbody>
+            </table>
+        {{ $roles->links() }}
+    </div>
 </x-app-layout>

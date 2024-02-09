ae42a06fd02ab33e4e64f514eba4c8ffd737a816
Nicolas Schutz <90822955+NicolasSchutz73@users.noreply.github.com>
2024-02-09 19:51:13 +0100

    Creation de la migration pour la table teams

--- database/migrations/2024_02_09_180925_create_teams_table.php
+++ database/migrations/2024_02_09_180925_create_teams_table.php
@@ -13,7 +13,20 @@ return new class extends Migration
     {
         Schema::create('teams', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('category');
             $table->timestamps();
+        });
+
+        // table pivot pour gérer la relation Many to Many entre les équipes et les users
+        Schema::create('team_user', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('team_id');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('team_id')->references('id')->on('teams')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
@@ -22,6 +35,7 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::dropIfExists('team_user');
         Schema::dropIfExists('teams');
     }
 };

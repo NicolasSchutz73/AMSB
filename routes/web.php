c7a78ef7963c89f35e60e625c2e190fe44bd013a
Nicolas Schutz <90822955+NicolasSchutz73@users.noreply.github.com>
2024-03-12 07:52:25 +0100

    update

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\GroupController;
 use App\Http\Controllers\ConversationController;
 use App\Http\Controllers\RoleController;
-//use App\Http\Controllers\SearchUserController;
+use App\Http\Controllers\SearchUserController;
 use App\Http\Controllers\UserMessController;
 use Illuminate\Support\Facades\Storage;
 use App\Http\Controllers\TeamController;

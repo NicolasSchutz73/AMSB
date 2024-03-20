1722cdbbd717139cff2be7b4a9e9f51221084d00
CroustiBapt <89194028+CobsiHD@users.noreply.github.com>
2024-03-20 18:02:20 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,9 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/js/chatRoom/notif.js',
+                'resources/js/chatRoom/affiche_user.js',
+                'resources/js/chatRoom/btn_image.js'
             ],
             refresh: true,
         }),
